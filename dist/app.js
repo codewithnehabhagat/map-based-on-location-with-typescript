848f960caf5cf15d9913f6d720c87ada41bd75b8
codewithnehabhagat <codewithnehabhagat@gmail.com>
2024-09-17 02:08:03 +0530

    map render based on address input

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,91 @@
 "use strict";
-console.log("Hello World!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+const ol_1 = require("ol");
+const layer_1 = require("ol/layer");
+const source_1 = require("ol/source");
+const Feature_1 = __importDefault(require("ol/Feature"));
+const Point_1 = __importDefault(require("ol/geom/Point"));
+const proj_1 = require("ol/proj");
+const style_1 = require("ol/style");
+const form = document.querySelector("form");
+const cityInput = document.getElementById("city");
+const stateInput = document.getElementById("state");
+const countryInput = document.getElementById("country");
+const postalCodeInput = document.getElementById("postalcode");
+var AddressType;
+(function (AddressType) {
+    AddressType["CITY"] = "city";
+    AddressType["STATE"] = "state";
+    AddressType["COUNTRY"] = "country";
+    AddressType["POSTCODE"] = "postcode";
+})(AddressType || (AddressType = {}));
+function getCoordinates() {
+    return axios_1.default.get(`https://nominatim.openstreetmap.org/search?city=${cityInput.value}&state=${stateInput.value}&country=${countryInput.value}&postalcode=${postalCodeInput.value}&format=json&polygon=1&addressdetails=1`);
+}
+function getZoomBasedOnAddressType(type) {
+    switch (type) {
+        case AddressType.COUNTRY:
+            return 3;
+        case AddressType.STATE:
+            return 5;
+        case AddressType.CITY:
+            return 6;
+        case AddressType.POSTCODE:
+            return 10;
+        default:
+            return 10;
+    }
+}
+function searchAddressHandler(event) {
+    event.preventDefault();
+    document.getElementById("response").innerHTML = "";
+    document.getElementById("map").innerHTML = "";
+    getCoordinates().then((response) => {
+        const data = response === null || response === void 0 ? void 0 : response.data;
+        if (data.length > 0) {
+            const relevantEntryToDisplay = data[0];
+            document.getElementById("response").innerHTML =
+                relevantEntryToDisplay.display_name;
+            const coord = (0, proj_1.fromLonLat)([
+                Number.parseFloat(relevantEntryToDisplay.lon),
+                Number.parseFloat(relevantEntryToDisplay.lat),
+            ]);
+            const marker = new Feature_1.default({
+                geometry: new Point_1.default(coord),
+            });
+            const markerLayer = new layer_1.Vector({
+                source: new source_1.Vector({
+                    features: [marker],
+                }),
+                style: new style_1.Style({
+                    image: new style_1.Icon({
+                        anchor: [0.5, 1],
+                        src: "./assets/marker.svg",
+                    }),
+                }),
+            });
+            new ol_1.Map({
+                layers: [
+                    new layer_1.Tile({
+                        source: new source_1.OSM(),
+                    }),
+                    markerLayer,
+                ],
+                target: "map",
+                view: new ol_1.View({
+                    center: coord,
+                    zoom: getZoomBasedOnAddressType(relevantEntryToDisplay.addresstype),
+                }),
+            });
+        }
+        else {
+            document.getElementById("response").innerHTML = "No Records Found.";
+        }
+    });
+}
+form.addEventListener("submit", searchAddressHandler);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,107 @@
-console.log("Hello World!");
+import axios, { AxiosResponse } from "axios";
+import { Map, View } from "ol";
+import { Tile as TileLayer, Vector as LayerVector } from "ol/layer";
+import { OSM, Vector as SourceVector } from "ol/source";
+import Feature from "ol/Feature";
+import Point from "ol/geom/Point";
+import { fromLonLat } from "ol/proj";
+import { Icon, Style } from "ol/style";
+
+const form = document.querySelector("form")!;
+
+const cityInput = document.getElementById("city")! as HTMLInputElement;
+const stateInput = document.getElementById("state")! as HTMLInputElement;
+const countryInput = document.getElementById("country")! as HTMLInputElement;
+const postalCodeInput = document.getElementById(
+  "postalcode"
+)! as HTMLInputElement;
+
+enum AddressType {
+  CITY = "city",
+  STATE = "state",
+  COUNTRY = "country",
+  POSTCODE = "postcode",
+}
+type CoordResponse = {
+  place_id: number;
+  display_name: string;
+  lon: string;
+  lat: string;
+  addresstype: AddressType;
+};
+
+function getCoordinates(): Promise<AxiosResponse<CoordResponse[]>> {
+  return axios.get(
+    `https://nominatim.openstreetmap.org/search?city=${cityInput.value}&state=${stateInput.value}&country=${countryInput.value}&postalcode=${postalCodeInput.value}&format=json&polygon=1&addressdetails=1`
+  );
+}
+
+function getZoomBasedOnAddressType(type: AddressType) {
+  switch (type) {
+    case AddressType.COUNTRY:
+      return 3;
+    case AddressType.STATE:
+      return 5;
+    case AddressType.CITY:
+      return 6;
+    case AddressType.POSTCODE:
+      return 10;
+
+    default:
+      return 10;
+  }
+}
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  document.getElementById("response")!.innerHTML = "";
+  document.getElementById("map")!.innerHTML = "";
+
+  getCoordinates().then((response) => {
+    const data = response?.data;
+    if (data.length > 0) {
+      const relevantEntryToDisplay = data[0];
+      document.getElementById("response")!.innerHTML =
+        relevantEntryToDisplay.display_name;
+
+      const coord = fromLonLat([
+        Number.parseFloat(relevantEntryToDisplay.lon),
+        Number.parseFloat(relevantEntryToDisplay.lat),
+      ]);
+
+      const marker = new Feature({
+        geometry: new Point(coord),
+      });
+
+      const markerLayer = new LayerVector({
+        source: new SourceVector({
+          features: [marker],
+        }),
+        style: new Style({
+          image: new Icon({
+            anchor: [0.5, 1],
+            src: "./assets/marker.svg",
+          }),
+        }),
+      });
+
+      new Map({
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+          markerLayer,
+        ],
+        target: "map",
+        view: new View({
+          center: coord,
+          zoom: getZoomBasedOnAddressType(relevantEntryToDisplay.addresstype),
+        }),
+      });
+    } else {
+      document.getElementById("response")!.innerHTML = "No Records Found.";
+    }
+  });
+}
+
+form.addEventListener("submit", searchAddressHandler);
